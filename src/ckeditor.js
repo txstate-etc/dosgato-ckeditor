15d3ea1b52b2b5ebae6c0bfa5b2d4e7920576816
kevinpena <kep85@txstate.edu>
2022-12-12 09:34:39 -0600

    adding plugins to base editor

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -2,90 +2,129 @@
  * @license Copyright (c) 2014-2022, CKSource Holding sp. z o.o. All rights reserved.
  * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
  */
-import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor.js';
-import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat.js';
-import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote.js';
-import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold.js';
-import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials.js';
-import Heading from '@ckeditor/ckeditor5-heading/src/heading.js';
-import { Image, ImageCaption, ImageStyle, ImageToolbar, ImageResize } from '@ckeditor/ckeditor5-image';
-import Indent from '@ckeditor/ckeditor5-indent/src/indent.js';
-import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic.js';
-import Link from '@ckeditor/ckeditor5-link/src/link.js';
-import List from '@ckeditor/ckeditor5-list/src/list.js';
-import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph.js';
-import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice.js';
-import { SourceEditing } from '@ckeditor/ckeditor5-source-editing';
-import Table from '@ckeditor/ckeditor5-table/src/table.js';
-import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar.js';
-import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation.js';
-import AssetBrowser from './AssetBrowser.js';
+import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor.js'
+import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat.js'
+import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote.js'
+import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold.js'
+import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials.js'
+import Font from '@ckeditor/ckeditor5-font/src/font'
+import Heading from '@ckeditor/ckeditor5-heading/src/heading.js'
+import HorizontalLine from '@ckeditor/ckeditor5-horizontal-line/src/horizontalline'
+import {
+  Image,
+  ImageCaption,
+  ImageStyle,
+  ImageToolbar,
+  ImageResize
+} from '@ckeditor/ckeditor5-image'
+import Indent from '@ckeditor/ckeditor5-indent/src/indent.js'
+import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic.js'
+import Link from '@ckeditor/ckeditor5-link/src/link.js'
+import List from '@ckeditor/ckeditor5-list/src/list.js'
+import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph.js'
+import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice.js'
+import RemoveFormat from '@ckeditor/ckeditor5-remove-format/src/removeformat'
+import { SourceEditing } from '@ckeditor/ckeditor5-source-editing'
+import SpecialCharacters from '@ckeditor/ckeditor5-special-characters/src/specialcharacters'
+import SpecialCharactersEssentials from '@ckeditor/ckeditor5-special-characters/src/specialcharactersessentials'
+import Table from '@ckeditor/ckeditor5-table/src/table.js'
+import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar.js'
+import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation.js'
+import Undo from '@ckeditor/ckeditor5-undo/src/undo'
+import AssetBrowser from './AssetBrowser.js'
 
 class Editor extends ClassicEditor {}
 
 // Plugins to include in the build.
 Editor.builtinPlugins = [
-	Autoformat,
-	BlockQuote,
-	Bold,
-	Essentials,
-	Heading,
-	Image,
-	ImageCaption,
-	ImageResize,
-	ImageStyle,
-	ImageToolbar,
-	Indent,
-	Italic,
-	Link,
-	List,
-	Paragraph,
-	PasteFromOffice,
-	SourceEditing,
-	Table,
-	TableToolbar,
-	TextTransformation,
-	AssetBrowser
-];
+  Autoformat,
+  BlockQuote,
+  Bold,
+  Essentials,
+  Font,
+  Heading,
+  HorizontalLine,
+  Image,
+  ImageCaption,
+  ImageResize,
+  ImageStyle,
+  ImageToolbar,
+  Indent,
+  Italic,
+  Link,
+  List,
+  Paragraph,
+  PasteFromOffice,
+  RemoveFormat,
+  SourceEditing,
+  SpecialCharacters,
+  SpecialCharactersEssentials,
+  Table,
+  TableToolbar,
+  TextTransformation,
+  Undo,
+  AssetBrowser
+]
 
 // Editor configuration.
 Editor.defaultConfig = {
-	toolbar: {
-		items: [
-			'heading',
-			'|',
-			'bold',
-			'italic',
-			'link',
-			'bulletedList',
-			'numberedList',
-			'|',
-			'outdent',
-			'indent',
-			'|',
-			'assetBrowserImage',
-			'blockQuote',
-			'sourceEditing'
-		]
-	},
-	language: 'en',
-	image: {
-		toolbar: [
-			'imageStyle:inline',
-			'imageStyle:wrapText',
-			'imageStyle:breakText',
-			'|',
-			'toggleImageCaption',
-			'imageTextAlternative'
-		]
-	},
-	table: {
-		contentToolbar: [
-			'tableColumn',
-			'tableRow',
-			'mergeTableCells'
-		]
-	}
-};
+  toolbar: {
+    items: [
+      'bold',
+      'italic',
+      'horizontalLine',
+      'blockQuote',
+      'removeFormat',
+      'specialCharacters',
+      '|',
+      'link',
+      'undo',
+      'redo',
+      '|',
+      'sourceEditing',
+      '-',
+      'numberedList',
+      'bulletedList',
+      'indent',
+      'outdent',
+      '|',
+      'heading',
+      'assetBrowserImage',
+      '|',
+      'fontColor'
+    ],
+    shouldNotGroupWhenFull: true
+  },
+  heading: {
+    options: [
+      { title: 'Paragraph', model: 'paragraph', class: 'ck-heading_paragraph' },
+      { title: 'Title', view: 'h2', model: 'heading2', class: 'ck-heading_heading2' },
+      { title: 'Subtitle', view: 'h3', model: 'heading3', class: 'ck-heading_heading3' },
+      { title: 'Subsubtitle', view: 'h4', model: 'heading4', class: 'ck-heading_heading4' },
+      { title: 'Preformatted Text', view: 'pre', model: 'preformattedText' },
+      { title: 'Superscript', view: 'sup', model: 'superscript' },
+      { title: 'Subscript', view: 'sub', model: 'subscript' },
+      { title: 'Strike Out', view: 's', model: 'strikeout' },
+      { title: 'Computer Code', view: 'code', model: 'code' }
+    ]
+  },
+  language: 'en',
+  image: {
+    toolbar: [
+      'imageStyle:inline',
+      'imageStyle:wrapText',
+      'imageStyle:breakText',
+      '|',
+      'toggleImageCaption',
+      'imageTextAlternative'
+    ]
+  },
+  table: {
+    contentToolbar: ['tableColumn', 'tableRow', 'mergeTableCells']
+  },
+  fontColor: {
+    colors: []
+  }
+}
 
-export default Editor;
+export default Editor
