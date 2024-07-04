b462e9bdda09cd6a0da582b0c7ab2e48b2ae895f
Anne Faber <anne.faber@txstate.edu>
2024-07-03 19:37:23 -0500

    Add large text style to headings menu

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -201,6 +201,16 @@ const defaultConfig = {
   heading: {
     options: [
       { title: 'Paragraph', model: 'paragraph', class: 'ck-heading_paragraph' },
+      {
+        title: 'Paragraph - Large Text',
+        model: 'paragraphLarge',
+        view: {
+          name: 'p',
+          classes: 'large-text'
+        },
+        class: 'ck-heading_paragraph_large',
+        converterPriority: 'high'
+      },
       { title: 'Title', view: 'h2', model: 'heading2', class: 'ck-heading_heading2' },
       { title: 'Subtitle', view: 'h3', model: 'heading3', class: 'ck-heading_heading3' },
       { title: 'Subsubtitle', view: 'h4', model: 'heading4', class: 'ck-heading_heading4' },
